8ce21ff1c098a3632447b3205109a3fa2b272ec5
Aluno <me@me.com>
2013-07-13 17:13:56 -0300

    ultima implementacao

--- src/br/ufcg/ppgcc/compor/jcf/experimento/fachada/Etapa1RenataFacade.java
+++ src/br/ufcg/ppgcc/compor/jcf/experimento/fachada/Etapa1RenataFacade.java
@@ -6,32 +6,48 @@ import java.util.List;
 import java.util.Map;
 
 import br.ufcg.ppgcc.compor.jcf.experimento.util.CalculoImpostoRenda;
+import br.ufcg.ppgcc.compor.jcf.experimento.util.Validacao;
 
 public class Etapa1RenataFacade implements FachadaExperimento {
 	
-	//Inicio da etapa 13:30
-	//Témino da etapa 15:44
+	//Inicio da etapa1 13:30
+	//Témino da etapa1 15:44
+	
+	//Inicio da etapa2 16:20
 
 	CalculoImpostoRenda calculo = new CalculoImpostoRenda();
+	Validacao validar = new Validacao();
 	List<Titular> titulares = new ArrayList<Titular>();
 
 	HashMap<Titular, List<FontePagadora>> mapaFontes = new HashMap<Titular, List<FontePagadora>>();
 	HashMap<Titular, List<Dependente>> mapaDependentes = new HashMap<Titular, List<Dependente>>();
 
-	@Override
+	
 	public void criarNovoTitular(Titular titular) {
+		
+		if (!validar.obrigatorio(titular.getNome())|| validar.obrigatorio(titular.getCpf())){
+			 throw new ExcecaoImpostoDeRenda ("Campo obrigatório");
+		}
+		
 		titulares.add(titular);
 	}
 
-	@Override
+	
 	public List<Titular> listarTitulares() {
 
 		return titulares;
 	}
 
-	@Override
+	
 	public void criarFontePagadora(Titular titular, FontePagadora fonte) {
-
+		
+		if(!validar.obrigatorio(fonte.getNome())|| !validar.obrigatorio(fonte.getCpfCnpj())){
+			 throw new ExcecaoImpostoDeRenda ("Campo obrigatório");
+		}
+		if (fonte.getRendimentoRecebidos()<=0){
+			 throw new ExcecaoImpostoDeRenda ("Campo obrigatório");
+		}
+		
 		if (mapaFontes.get(titular) == null) {
 			mapaFontes.put(titular, new ArrayList<FontePagadora>());
 		}
@@ -39,7 +55,7 @@ public class Etapa1RenataFacade implements FachadaExperimento {
 
 	}
 
-	@Override
+	
 	public Resultado declaracaoCompleta(Titular titular) {
 
 		Resultado resultado = new Resultado();
@@ -57,8 +73,16 @@ public class Etapa1RenataFacade implements FachadaExperimento {
 		return resultado;
 	}
 
-	@Override
+
 	public void criarDependente(Titular titular, Dependente dependente) {
+		
+		if (dependente.getTipo()<=0){
+			 throw new ExcecaoImpostoDeRenda ("Campo obrigatório");
+		}
+		
+		if(!validar.obrigatorio(dependente.getNome())|| !validar.obrigatorio(dependente.getCpf())){
+			 throw new ExcecaoImpostoDeRenda ("Campo obrigatório");
+		}
 
 		if (mapaDependentes.get(titular) == null) {
 			mapaDependentes.put(titular, new ArrayList<Dependente>());
@@ -67,33 +91,36 @@ public class Etapa1RenataFacade implements FachadaExperimento {
 
 	}
 
-	@Override
+	
 	public List<FontePagadora> listarFontes(Titular titular) {
 
 		return mapaFontes.get(titular);
 	}
 
-	@Override
+	
 	public List<Dependente> listarDependentes(Titular titular) {
+		
+		
 
 		return mapaDependentes.get(titular);
 	}
 
-	@Override
+	
 	public void criarGastoDedutivel(Titular titular, Pessoa realizador,
 			GastoDedutivel gastoDedutivel) {
 		// TODO Auto-generated method stub
 		
+		
 	}
 
-	@Override
+	
 	public List<GastoDedutivel> listarGastosDedutiveis(Titular titular,
 			Pessoa realizador) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+	
 	public Resultado relatorioSimplificado(Titular titular) {
 		// TODO Auto-generated method stub
 		return null;
